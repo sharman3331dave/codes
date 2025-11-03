c3afb3229ae67ee4fc1c8e201ed4048f3b00b39d
sharman3331dave <sharman3331dave@gmail.com>
2025-11-03 11:07:34 +0530

    Add files via upload

---
+++ day22q1.c
@@ -0,0 +1,33 @@
+//Write a program to check if a number is a strong number.
+#include <stdio.h>
+
+int factorial(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int number, temp, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    temp = number;
+
+    while (temp > 0) {
+        int digit = temp % 10;
+        sum += factorial(digit);
+        temp = temp / 10;
+    }
+
+    if (sum == number) {
+        printf("Strong number\n");
+    } else {
+        printf("Not strong number\n");
+    }
+
+    return 0;
+}
---
+++ day22q2.c
@@ -0,0 +1,25 @@
+//Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    float sum = 0.0;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    int numerator = 1;
+    int denominator = 1;
+
+    for (int i = 1; i <= n; i++) {
+        sum += (float)numerator / denominator;
+
+        numerator += 2;
+        denominator += 2;
+    }
+
+    printf("Approximate sum: %.1f\n", sum);
+
+    return 0;
+}
